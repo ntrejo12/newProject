055903049d8756ad9c42427c853b092dd679c56d
ntrejo12 <naydelintrejo12@gmail.com>
2024-04-17 16:58:35 -0400

    Draw grid

--- test_code.py
+++ test_code.py
@@ -2,25 +2,42 @@ import pygame
 import sys
 
 # Initialize Pygame
+# will go in main function?
 pygame.init()
 
 # Set up the display
-screen = pygame.display.set_mode((750, 750))
+width = 750
+height = 750
+screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Sudoku Grid")
 
 # Define colors
 PINK = (255, 182, 193)
 BLACK = (0, 0, 0)
 
+def __init__(self, width, height, screen, difficulty):
+    self.width = width
+    self.height = height
+    self.screen = screen
+    self.difficulty = difficulty
+
 def draw():
     # gives us a pink background
     screen.fill(PINK)
+    # draws outside rectangle
     pygame.draw.rect(screen, BLACK, pygame.Rect(15, 15, 720, 720), 10)
     i = 1
     while (i * 80) < 720:
-        pygame.draw.line(screen, BLACK, pygame.Vector2(((i * 80) + 15), 15), pygame.Vector2(((i * 80) + 15), 15), 5)
+        if i % 3 != 0:
+            line_width = 5
+        else:
+            line_width = 10
 
-
+        # draws vertical lines
+        pygame.draw.line(screen, BLACK, pygame.Vector2(((i * 80) + 15), 15), pygame.Vector2(((i * 80) + 15), 735), line_width)
+        # draws horizontal lines
+        pygame.draw.line(screen, BLACK, pygame.Vector2(15, ((i * 80) + 15)), pygame.Vector2(735, ((i * 80) + 15)), line_width)
+        i += 1
 
 def game_loop():
     while True:
